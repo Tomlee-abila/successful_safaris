0a5e313ce30726d9cc8c982add04dc6ef123f9cf
TomleeAbila <tomlee@mtaka.co.ke>
2026-06-22 00:05:17 +0300

    Update STATIC_ROOT and MEDIA_ROOT to use environment variables

--- backend/safari_backend/settings.py
+++ backend/safari_backend/settings.py
@@ -91,7 +91,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = Path(os.environ.get('DJANGO_STATIC_ROOT', BASE_DIR / 'staticfiles'))
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
@@ -106,7 +106,7 @@ STORAGES = {
 }
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = Path(os.environ.get('DJANGO_MEDIA_ROOT', BASE_DIR / 'media'))
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
